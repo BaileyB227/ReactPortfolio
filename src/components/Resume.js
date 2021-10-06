fa00e84179c523e168e5cd5a4c7af96b774ba6d9
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-10-06 12:04:19 -0500

    fixed css

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -12,7 +12,7 @@ function Resume() {
 				</a>
 			</div>
 			<div>
-				<h5>Front-End Proficiencies</h5>
+				<h5>Proficiencies</h5>
 				<ol>
 					<li>HTML5</li>
 					<li>CSS3</li>
@@ -20,38 +20,17 @@ function Resume() {
 					<li>jQuery</li>
 					<li>Bootstrap</li>
 					<li>React</li>
-					<li>version control/Git</li> 
-					<li>responsive design</li>
-				</ol>
-				<br></br>
-				<h5>Back-End Proficiencies</h5>
-				<ol>
 					<li>APIs</li>
 					<li>NodeJS</li>
 					<li>Express</li>
-					<li>Model View Controller (MVC)</li>
-					<li>REST</li>
+					<li>MVC</li>
 					<li>Progressive Web Applications (PWA)</li>
-				</ol>
-				<br></br>
-				<h5>Dev Tool Proficiencies</h5>
-				<ol>
-					<li>Git</li>
-					<li>npm</li>
-					<li>Jest</li>
-					<li>Webpack</li>
-				</ol>
-				<br></br>
-				<h5>Database Proficiencies</h5>
-				<ol>
 					<li>MySQL</li>
-					<li>Sequelize</li>
 					<li>NoSQL</li>
-					<li>SQLite</li>
-					<li>GraphQL</li>
 					<li>MongoDB</li>
 					<li>Mongoose</li>
 				</ol>
+				
 			</div>
 		</section>
 	);
