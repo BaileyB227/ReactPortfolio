df97e09a2e89ce7d7025058ca4556400101d607e
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-10-06 09:59:25 -0500

    boiler plus edits

---
+++ src/components/Footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<div>
+				<a
+					href="https://github.com/BaileyB227"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../assets/githublogo.png")}
+						alt="Github"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://www.linkedin.com/in/BaileyB227/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../assets/linkedinlogo.png")}
+						alt="LinkedIn"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://mail.google.com/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../assets/gmaillogo.png")}
+						alt="Gmail"
+						className="logo"
+					></img>
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
